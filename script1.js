dfe6f0b8b9970e9bab428cb526d9afda88b1cb1d
Yuri <example@gmail.com>
2024-06-09 18:01:40 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,20 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("You have 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('What you name?', '');
+// answers[1] = prompt('What you last name?', '');
+// answers[2] = prompt('What you age?', '');
+
+// document.write(answers);
+console.log(typeof(answer));
+console.log(typeof(null));
+
